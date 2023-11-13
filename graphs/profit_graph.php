c8d0ae16542318899e38292db93a13af190a4c38
Pahad08 <mastahpahad@gmail.com>
2023-11-13 20:41:24 +0800

    fixed bugs

--- graphs/profit_graph.php
+++ graphs/profit_graph.php
@@ -33,31 +33,31 @@ if ($row1['total_expense'] - $row2['sale'] < 0) {
 </div>
 
 <script>
-    google.charts.load('current', {
-        'packages': ['corechart']
-    });
-    google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {
+    'packages': ['corechart']
+});
+google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
+function drawChart() {
 
-        const data = google.visualization.arrayToDataTable([
-            ['Profit in this month', 'Profit'],
-            <?php
+    const data = google.visualization.arrayToDataTable([
+        ['Profit in this month', 'Profit'],
+        <?php
             $month = date('F');
             echo "['" . "Profit In " . $month . "', " . $profit . "],";
             ?>
-        ]);
+    ]);
 
-        const options = {
-            animation: {
-                duration: 1500,
-                easing: 'out',
-                startup: true
-            }
-        };
+    const options = {
+        animation: {
+            duration: 1500,
+            easing: 'out',
+            startup: true
+        }
+    };
 
-        const chart = new google.visualization.BarChart(document.getElementById('profits'));
-        chart.draw(data, options);
+    const chart = new google.visualization.BarChart(document.getElementById('profits'));
+    chart.draw(data, options);
 
-    }
+}
 </script>
--- graphs/sale_perproduct.php
+++ graphs/sale_perproduct.php
@@ -20,32 +20,32 @@ $result = mysqli_stmt_get_result($stmt);
 </div>
 
 <script>
-google.charts.load('current', {
-    'packages': ['corechart']
-});
-google.charts.setOnLoadCallback(drawChart);
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
+    function drawChart() {
 
-    const data = google.visualization.arrayToDataTable([
-        ['Product Name', 'Total'],
-        <?php
+        const data = google.visualization.arrayToDataTable([
+            ['Product Name', 'Total'],
+            <?php
             while ($row = mysqli_fetch_array($result)) {
                 echo "['" . $row['name'] . "', " . $row['total'] . "], ";
             }
             ?>
-    ]);
+        ]);
 
-    const options = {
-        animation: {
-            duration: 1500,
-            easing: 'out',
-            startup: true
-        }
-    };
+        const options = {
+            animation: {
+                duration: 1500,
+                easing: 'out',
+                startup: true
+            }
+        };
 
-    const chart = new google.visualization.ColumnChart(document.getElementById('product-sale'));
-    chart.draw(data, options);
+        const chart = new google.visualization.ColumnChart(document.getElementById('product-sale'));
+        chart.draw(data, options);
 
-}
+    }
 </script>
