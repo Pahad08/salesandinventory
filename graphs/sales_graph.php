38edd41907b5ab4b281ac1067c18ede9fc58bbbb
Pahad08 <mastahpahad@gmail.com>
2023-11-17 22:19:30 +0800

    fixed weekly sales

--- graphs/sales_graph.php
+++ graphs/sales_graph.php
@@ -1,14 +1,39 @@
 <?php
 include 'openconn.php';
 
-$sql = "SELECT sum(sales.quantity * products.price) as sale, DAYNAME(CURRENT_DATE) as dayname
+function ArrayAdd($index, $row, $weeks, $weekly_sales)
+{
+    $sub_array = array();
+    array_push($sub_array, $weeks[$index]);
+    array_push($sub_array, $row);
+    array_push($weekly_sales, $sub_array);
+    return $weekly_sales;
+}
+
+$sql = "SELECT sum(sales.quantity * products.price) as sale
 from sales
 inner JOIN products on sales.product_id = products.product_id 
-where WEEK(sales.sale_date) = WEEk(CURRENT_DATE);";
+where WEEK(sales.sale_date) = WEEk(CURRENT_DATE) or WEEK(sales.sale_date) = WEEK(CURRENT_DATE)-1
+group by WEEK(sales.sale_date);";
 $stmt = mysqli_prepare($conn, $sql);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
+$total_rows = mysqli_num_rows($result);
 $row = mysqli_fetch_array($result);
+
+$weekly_sales = array();
+$weeks = array("Last Week", "This Week");
+$index = ($total_rows == 1) ? 1 : 0;
+
+if ($total_rows == 1) {
+    $weekly_sales =  ArrayAdd($index, $row['sale'], $weeks, $weekly_sales);
+} else {
+    while ($row) {
+        $weekly_sales = ArrayAdd($index, $row['sale'], $weeks, $weekly_sales);
+        $index++;
+        $row = mysqli_fetch_array($result);
+    }
+}
 
 ?>
 
@@ -20,30 +45,35 @@ $row = mysqli_fetch_array($result);
 </div>
 
 <script>
-google.charts.load('current', {
-    'packages': ['corechart']
-});
-google.charts.setOnLoadCallback(drawChart);
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
+    function drawChart() {
 
-    const data = google.visualization.arrayToDataTable([
-        ['Sales', 'Weekly Sales'],
-        <?php
-            echo "['" . $row['dayname'] . "', " . $row['sale'] . "], ";
+        const data = google.visualization.arrayToDataTable([
+            ['Sales', 'Weekly Sales'],
+            <?php
+            for ($i = 0; $i < count($weekly_sales); $i++) {
+                for ($j = 0; $j < 1; $j++) {
+                    echo "['" . $weekly_sales[$i][$j] . "', " . $weekly_sales[$i][$j + 1] . "], ";
+                }
+            }
+
             ?>
-    ]);
+        ]);
 
-    const options = {
-        title: 'Weekly Sales',
-        animation: {
-            duration: 1500,
-            easing: 'out',
-            startup: true
-        }
-    };
-    const chart = new google.visualization.BarChart(document.getElementById('sale'));
-    chart.draw(data, options);
+        const options = {
+            title: 'Weekly Sales',
+            animation: {
+                duration: 1500,
+                easing: 'out',
+                startup: true
+            }
+        };
+        const chart = new google.visualization.BarChart(document.getElementById('sale'));
+        chart.draw(data, options);
 
-}
+    }
 </script>
