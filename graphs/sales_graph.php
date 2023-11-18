cc9f4ab86aad4c7871c838e79f0e036ab64f2401
Pahad08 <mastahpahad@gmail.com>
2023-11-18 11:22:46 +0800

    changes in graph

--- graphs/sales_graph.php
+++ graphs/sales_graph.php
@@ -1,40 +1,14 @@
 <?php
 include 'openconn.php';
 
-function ArrayAdd($index, $row, $weeks, $weekly_sales)
-{
-    $sub_array = array();
-    array_push($sub_array, $weeks[$index]);
-    array_push($sub_array, $row);
-    array_push($weekly_sales, $sub_array);
-    return $weekly_sales;
-}
-
-$sql = "SELECT sum(sales.quantity * products.price) as sale
+$sql = "SELECT (sales.quantity * products.price) as sale, sales.sale_date
 from sales
 inner JOIN products on sales.product_id = products.product_id 
-where WEEK(sales.sale_date) = WEEk(CURRENT_DATE) or WEEK(sales.sale_date) = WEEK(CURRENT_DATE)-1
-group by WEEK(sales.sale_date);";
+where WEEK(sales.sale_date) = WEEK(CURRENT_DATE)
+ORDER by DAY(sales.sale_date);";
 $stmt = mysqli_prepare($conn, $sql);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
-$total_rows = mysqli_num_rows($result);
-$row = mysqli_fetch_array($result);
-
-$weekly_sales = array();
-$weeks = array("Last Week", "This Week");
-$index = ($total_rows == 1) ? 1 : 0;
-
-if ($total_rows == 1) {
-    $weekly_sales =  ArrayAdd($index, $row['sale'], $weeks, $weekly_sales);
-} else {
-    while ($row) {
-        $weekly_sales = ArrayAdd($index, $row['sale'], $weeks, $weekly_sales);
-        $index++;
-        $row = mysqli_fetch_array($result);
-    }
-}
-
 ?>
 
 <div id="sales">
@@ -55,10 +29,8 @@ if ($total_rows == 1) {
         const data = google.visualization.arrayToDataTable([
             ['Sales', 'Weekly Sales'],
             <?php
-            for ($i = 0; $i < count($weekly_sales); $i++) {
-                for ($j = 0; $j < 1; $j++) {
-                    echo "['" . $weekly_sales[$i][$j] . "', " . $weekly_sales[$i][$j + 1] . "], ";
-                }
+            while ($row = mysqli_fetch_array($result)) {
+                echo "['" . $row['sale_date'] . "', " . $row['sale'] . "], ";
             }
 
             ?>
