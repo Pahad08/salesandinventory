2e3ada54c16694e42d05b827cfb55fa4c2a03496
Pahad08 <mastahpahad@gmail.com>
2023-11-22 00:38:13 +0800

    fixed error

--- delete/deleteacc.php
+++ delete/deleteacc.php
@@ -9,20 +9,16 @@ if (!isset($_SESSION["admin"]) && !isset($_SESSION["admin_username"])) {
 if (!empty($_POST['account_id'])) {
     include '../openconn.php';
     $account_ids = $_POST['account_id'];
-    $null = null;
 
     foreach ($account_ids as $id) {
-        $stmt1 = mysqli_prepare($conn, "UPDATE suppliers SET account_id = ? where account_id = ?;");
-        mysqli_stmt_bind_param($stmt1, "si", $null, $id);
-        mysqli_stmt_execute($stmt1);
-
-
-        $stmt2 = mysqli_prepare($conn, "UPDATE workers SET account_id = ? where account_id = ?;");
-        mysqli_stmt_bind_param($stmt2, "si", $null, $id);
-        mysqli_stmt_execute($stmt2);
-
+        $stmt1 = mysqli_prepare($conn, "DELETE FROM suppliers WHERE account_id = ?;");
+        $stmt2 = mysqli_prepare($conn, "DELETE FROM workers WHERE account_id = ?;");
         $stmt3 = mysqli_prepare($conn, "DELETE FROM accounts WHERE account_id = ?;");
+        mysqli_stmt_bind_param($stmt1, "i", $id);
+        mysqli_stmt_bind_param($stmt2, "i", $id);
         mysqli_stmt_bind_param($stmt3, "i", $id);
+        mysqli_stmt_execute($stmt1);
+        mysqli_stmt_execute($stmt2);
         mysqli_stmt_execute($stmt3);
     }
     mysqli_close($conn);
